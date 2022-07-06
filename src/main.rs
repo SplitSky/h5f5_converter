faf9a928963ee485a1636f56e29bdf84c257746c
Splitsky <opop.neska60@gmail.com>
2022-07-06 15:57:10 +0100

    change of direction in the development. checkpoint to see if multiple data set types will be better

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,135 @@
 #[cfg(feature = "blosc")]
 use hdf5::filters::blosc_set_nthreads;
 use hdf5::{File, H5Type, Result, Selection};
-use ndarray::{arr2,s};
+use ndarray::{arr2,s, Dimension};
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
 //#[derive(H5Type, Clone, PartialEq, Debug)] // register with HDF5
 //#[repr(C)]
 
-#[derive(Serialize, Deserialize)]
-pub struct FileNode {
+//#[derive(H5Type, Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct FileNodeStruct {
     filename: String,
-    groups: Vec<hdf5::Group>
+    file: hdf5::File,
+    groups: Vec<GroupStruct>
 }
 
-impl FileNode {
-    pub fn new(&mut self, filename_init: String, groupslist: Vec<hdf5::Group>) -> Self {
-        Self {filename: filename_init, groups: groupslist}
+impl FileNodeStruct {
+    pub fn new(filename_init: String, file_init: hdf5::File) -> Self {
+        let file = file_init;
+        let filename = filename_init;
+        let groups = Vec::new();
+
+
+
+        let group_list = file.groups().unwrap();
+        
+        for entry in group_list {
+            let temp_name = entry.name(); // string
+            let temp_groups_child = entry.groups(); // Vec<h5 ... Group> 
+            let temp_datasets = entry.datasets(); // Vec<h5 ... Dataset>
+
+
+            groups.push(GroupStruct::new(entry.name(), entry.groups().unwrap(), entry.datasets().unwrap()))
+        }
+        Self {filename: filename_init, groups: groups, file: file}
     }
+
+    pub fn populate_groups() {
+
+    }
+
 }
 
 
 // end of file struct
 
-//pub struct Group {
-//    key: String,
-//    datasets: Vec<DataSet>,
-//    groups: Vec<Group>
-//}
+pub struct GroupStruct {
+    key: String,
+    datasets: Vec<DataSet_struct>,
+    groups: Vec<GroupStruct>
+}
 
-//impl Group {
-//    pub fn new(key: String, datasets: Vec<DataSet>, groups: Vec<Group>) -> Self {
-//        Self {key, datasets, groups}
-//    }
-//}
+impl GroupStruct {
+    pub fn new(key_init: String, h5_groups: Vec<hdf5::Group>, h5_datasets: Vec<hdf5::Dataset>) -> Self {
+        // passes in the associated groups that are children to this node
+        let key = key_init;
+        let groups = Vec::new();
+        for entry in h5_groups {
+            groups.push(GroupStruct::new(entry.name(), entry.groups().unwrap(), entry.datasets().unwrap()));
+        }
+        let datasets = Vec::new();
+        for entry in h5_datasets {
+            datasets.push(DataSet_struct::new())       
+        }
 
-//impl Group {
-//    pub fn add_dataset(&mut self, key_in: String, data_in: Vec<u32>, dimension: i64) { // appends a
-        // data set to a group
-        //let mut dataset_temp = DataSet { id: key_in, dim: dimension, data: data_in};
-//        self.datasets.push(DataSet { id: key_in, dim: dimension, data: data_in})
+        Self {key, datasets, groups}
+    }
 
-//    }
-//}
+}
+
+
 
 //#[derive(H5Type, Clone, PartialEq, Debug)] // register with HDF5
 // Data set struct
-//pub struct DataSet {
-//    id: String,
-//    dim: i64,
-//    data: Vec<u32>
-//}
+
+pub struct DataSet_struct {
+    id: String,
+    size: Vec<usize>,
+    dim: i8,
+    data_temp1: ndarray::Array1<f64>,
+    data_temp2: ndarray::Array2<f64>,
+    data_temp3: ndarray::ArrayD<f64>
+}
+
+enum Either<A,B> {
+    Single(A),
+    Double(B),
+    //Dynamic(C)
+}
+
+
+fn pick_data(dim: usize, data_temp1: ndarray::Array1<f64>, data_temp2: ndarray::Array2<f64>) ->Either<ndarray::Array1<f64>, ndarray::Array2<f64>> {
+    if dim == 1 {
+        Either::Single(data_temp1);
+    } else if dim == 2 {
+         Either::Double(data_temp2);
+    }
+}
+
+impl DataSet_struct {
+    pub fn new(name: String, dataSet: hdf5::Dataset) -> Self { // implement ndarray for handling n
+        // dimensional data - for now only implement 1d, 2d data
+        let id = name;
+        let size = dataSet.shape();
+        let dim = size.len();
+        // depending on data size read into
+        if size.len() == 1 {
+            let data_temp1 = dataSet.read_1d::<f64>().unwrap();
+        } else if size.len() == 2 {
+            let data_temp2 = dataSet.read_2d::<f64>().unwrap();
+        } //else if size.len() == 3 {
+        //    let data_temp3 = dataSet.read_dyn::<f64>().unwrap();
+        //}
+
+        Self {id, size, dim, data}
+    }
+
+
+    pub fn return_data(self) -> Either<ndarray::Array1<f64>, ndarray::Array2<f64> , ndarray::Array3<f64>> {
+        use Either::{Single, Double};
+
+        if self.dim == 1 {
+            Either::single(self.data_temp1);
+        } else if self.dim == 2 {
+            Either::double(self.data_temp2);
+        } else {
+            Either::dynamic(self.data_temp3);
+        }
+    }
+
+}
 
 //impl DataSet {
 //    pub fn new(&mut self, file: &File ,id: String, dim: i64, data: Vec<u32>, position: String) -> Self {
@@ -62,7 +141,6 @@ impl FileNode {
 //        Self {id, dim, data}
 //    }
 //}
-
 
 fn read_hdf5() -> Result<()> {
     
@@ -106,10 +184,10 @@ fn read_hdf5() -> Result<()> {
     let filename_init = "nanowires_data.h5";
     let file = File::open(filename_init).unwrap(); // open file for reading
     // created file object
-    let node = FileNode { filename: filename_init.to_string(), groups: file.groups().unwrap()};
+    let node = FileNodeStruct { filename: filename_init.to_string(), file: file};
     
     // convert the FileNode into a JSON file using Serde
-    let j = serde_json::to_string(&mut node).unwrap();
+    //let j = serde_json::to_string(&mut node).unwrap();
      
 
     Ok(())
